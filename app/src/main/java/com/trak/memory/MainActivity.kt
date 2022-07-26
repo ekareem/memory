cc13947297cee1ebfde40e0e17320ef8c170bb79
ekareem <emmanuel45kareem@gmail.com>
2022-07-26 08:38:37 -0400

    MEM-1 remove system bar

--- app/src/main/java/com/trak/memory/MainActivity.kt
+++ app/src/main/java/com/trak/memory/MainActivity.kt
@@ -3,26 +3,38 @@ package com.trak.memory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ui.Scaffold
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.trak.memory.ui.theme.MemoryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = true)
+            }
             MemoryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,14 +42,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Scaffold {
+        Box(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MemoryTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
---
+++ app/src/main/java/com/trak/memory/scene/game/viewModel/GameViewModel.kt
@@ -0,0 +1,5 @@
+package com.trak.memory.scene.game.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel()
