c489c726570939d2af1d860ddd0b7744d619e06c
ekareem <emmanuel45kareem@gmail.com>
2022-07-27 21:40:35 -0400

    MEM-4 image screen

--- app/src/main/java/com/trak/memory/scene/game/GameScene.kt
+++ app/src/main/java/com/trak/memory/scene/game/GameScene.kt
@@ -1,30 +1,38 @@
 package com.trak.memory.scene.game
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.trak.memory.R
+import com.trak.memory.scene.game.viewModel.GameViewModel
 import com.trak.memory.ui.theme.MemoryTheme
 
 @Composable fun GameScene(
-    modifier: Modifier = Modifier
+    gameViewModel: GameViewModel = GameViewModel()
 ) {
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        Text(
-            text = "GAME SCENE",
-            style = MaterialTheme.typography.titleLarge,
-        )
+    Box(modifier = Modifier, contentAlignment = Alignment.Center) {
+        painterResource(id = R.drawable.ic_set).run {
+            Image(
+                painter = this,
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = if (intrinsicSize.width > intrinsicSize.height)
+                    ContentScale.FillHeight else ContentScale.FillWidth
+            )
+        }
     }
 }
 
-@Preview(showSystemUi = true)
+@Preview()
 @Composable fun GameScenePreview() {
+
     MemoryTheme {
-        GameScene(
-            Modifier
-        )
+        GameScene()
     }
 }
